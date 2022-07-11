1f04956163f93745b6378d8ffb1fc5fd1c9f3fe4
hnsenchristiann <christiansensen00@gmail.com>
2022-07-11 13:24:30 +0700

    authors edit

--- src/views/author/Add.vue
+++ src/views/author/Add.vue
@@ -11,11 +11,10 @@
     >
       <form
         class="w-full max-w-full"
-        @submit.prevent="addAuthors"
       >
         <div class="flex items-center border-b border-teal-500 py-2 max-w-3xl">
           <input
-            v-model="name"
+            v-model="author.name"
             class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
             type="text"
             placeholder="Author's Name"
@@ -32,7 +31,7 @@
             </label>
             <input
               id="grid-first-name"
-              v-model="email"
+              v-model="author.email"
               class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
               type="text"
               placeholder="Enter Author's Email"
@@ -47,7 +46,7 @@
           >Description</label>
           <textarea
             id="exampleFormControlTextarea1"
-            v-model="description"
+            v-model="author.description"
             class="
                 form-control
                 block
@@ -73,9 +72,6 @@
           <p class="font-bold text-gray-700">
             Social Media Links :
           </p>
-          <button class="mt-3 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-            Add a new social media
-          </button>
           <div class="my-5">
             <div class="bg-white shadow shadow-2xl max-w-xl p-5">
               <p class="font-bold py-3">
@@ -83,7 +79,7 @@
               </p>
               <input
                 id="grid-first-name"
-                v-model="social_media_links.facebook"
+                v-model="author.social_media_links.facebook"
                 class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                 type="text"
                 placeholder="Enter Author's Social Media Link"
@@ -97,7 +93,7 @@
               </p>
               <input
                 id="grid-first-name"
-                v-model="social_media_links.twitter"
+                v-model="author.social_media_links.twitter"
                 class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                 type="text"
                 placeholder="Enter Author's Social Media Link"
@@ -106,37 +102,62 @@
             </div>
           </div>
 
-          <p class="font-bold text-gray-700">
-            Profile Photo :
-          </p>
-
-          <div class="max-w-xl mt-3">
-            <div>
-              <img
-                v-if="imageDataUrl"
-                class="preview"
-                height="268"
-                width="356"
-                :src="imageDataUrl"
+          <div>
+            <div class="max-w-xl mt-3">
+              <p class="font-bold text-gray-700">
+                Profile Image :
+              </p>
+              <label
+                class="flex justify-center w-full h-32 px-4 mt-2 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none"
               >
-              <input
-                ref="profilePictureRef"
-                type="file"
-                style="display:none"
-                @change="onFileChange"
-              >
-              <button
-                class="font-bold px-3 border-2 rounded"
-                @click="selectProfile"
-              >
-                Choose Profile Picture
-              </button>
+                <img
+                  v-if="imageDataUrl"
+                  :src="imageDataUrl"
+                >
+                <img
+                  v-else-if="coverLoaded"
+                  :src="author.profile_picture_url"
+                >
+                <span
+                  v-else
+                  class="flex items-center space-x-2"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    class="w-6 h-6 text-gray-600"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    stroke-width="2"
+                  >
+                    <path
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
+                    />
+                  </svg>
+                  <span class="font-medium text-gray-600">
+                    Drop files to Attach, or
+                    <span class="text-blue-600 underline">browse</span>
+                  </span>
+                </span>
+                <input
+                  accept="image/*"
+                  type="file"
+                  name="file_upload"
+                  class="hidden"
+                  @change="onFileChange"
+                >
+              </label>
             </div>
-          </div>
 
-          <button class="mt-5 bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded">
-            Save
-          </button>
+            <button
+              class="mt-5 bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 rounded"
+              @click.prevent="submitData"
+            >
+              Save
+            </button>
+          </div>
         </div>
       </form>
     </author-card>
@@ -150,61 +171,77 @@ import MainSection from '@/components/MainSection.vue'
 import AuthorCard from '@/views/author/component/AuthorCard.vue'
 import TitleBar from '@/components/TitleBar.vue'
 import HeroBar from '@/components/HeroBar.vue'
-import { collection, addDoc } from 'firebase/firestore'
-import firebase from '@/firebase/firebase'
-import { ProfilePicture } from '@/firebase/types'
+import Author from '@/firebase/Author.js'
+import handleError from '@/utils/handleError.js'
 // import { getStorage, ref } from "firebase/storage"
 export default {
 	data () {
 		return {
-			// social_media: [{
-			//   name: '',
-			//   link: ''
-			// }]
-			name: null,
-			email: null,
-			description: null,
-			social_media_links: {
-				facebook: null,
-				twitter: null
-			},
-			profile_picture_url: null,
-			imageDataUrl: null,
-			ProfilePicture: null
+      author: {
+        name: [],
+        email: [],
+        description: [],
+        social_media_links: {
+          facebook: [],
+          twitter: []
+        },
+      },
+			coverImage: null,
+			coverImageChanged: false,
+			imageDataUrl: null
 		}
 	},
 	methods: {
-		// addNewSocialMedia () {
-		//   this.social_media.push({
-		//     name: '',
-		//     link: ''
-		//   })
-		// },
-		// deleteSocialMedia (index) {
-		//   this.social_media.splice(index, 1)
-		// }
-		async addAuthors () {
-			const author = {
-				name: this.name,
-				email: this.email,
-				description: this.description,
-				social_media_links: {
-					facebook: this.social_media_links.facebook,
-					twitter: this.social_media_links.twitter
-				},
-				profile_picture_url: this.imageDataUrl
+		async submitData () {
+			try {
+				// eslint-disable-next-line camelcase
+				const { name, email, description, social_media_links: {facebook, twitter} } = this.author
+				const newAuthor = await Author.createDocument(
+					{
+						name,
+            email,
+            description,
+            social_media_links: {
+              facebook,
+              twitter
+            }
+					}
+				)
+				console.log(newAuthor)
+				if (this.coverImageChanged) {
+					try {
+						console.log('asdfasdf')
+						await newAuthor.adminUploadField('profile_picture_url', 'authors/' + newAuthor.id, this.coverImage)
+					} catch (err) {
+						await newAuthor.deleteDocument()
+						console.error('error... deleting...')
+						handleError(err, 'uploadFileError')
+						throw err
+					}
+				}
+				this.$toast.open({
+					message: 'Success!',
+					type: 'success',
+					duration: 5000,
+					dismissible: true,
+					position: 'bottom'
+				})
+			} catch (err) {
+				console.error(err)
+				this.$toast.open({
+					message: err.toString(),
+					type: 'error',
+					duration: 5000,
+					dismissible: true,
+					position: 'bottom'
+				})
 			}
-			const addData = await addDoc(collection(firebase.db, 'authors'), author)
-			this.imageDataUrl = await ProfilePicture.uploadField('profile_picture_url', '/profile_images' + this.ProfilePicture)
-			this.$router.push('/author')
-			console.log(addData)
+			this.$router.back()
 		},
 		onFileChange (event) {
-			this.ProfilePicture = event.target.files[0]
-			this.imageDataUrl = URL.createObjectURL(this.ProfilePicture)
-		},
-		selectProfile () {
-			this.$refs.profilePictureRef.click()
+			this.coverImage = event.target.files[0]
+			this.imageDataUrl = URL.createObjectURL(this.coverImage)
+			this.coverImageChanged = true
 		}
 	}
 }
